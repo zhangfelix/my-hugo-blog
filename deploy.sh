36f0a34f1631378d0597cd71b75b2c6de04eff40
felix <zhangfelixx@hotmail.com>
2021-07-30 16:49:51 +0800

    Use github actions.

--- deploy.sh
+++ deploy.sh
@@ -6,28 +6,6 @@ set -x
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
-# Build the project.
-hugo -t dream # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-
-# Back to the site source folder
-cd ..
-
 # Add changes to git.
 git add .
 
